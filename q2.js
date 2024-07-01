7ab0bba7f35c16205d5710bece860a199765b4d1
itspornpailin <pornpailin_j@cmu.ac.th>
2024-07-02 01:07:51 +0700

    commit

---
+++ q2.js
@@ -0,0 +1,13 @@
+function arrayStringify(a) {
+    for (var i = 0; i < a.length; i++) {
+        var text = a[i];
+        console.log(text);
+    }
+}
+var c1 = [1, 2, 3];
+var c2 = [10, 9, 8, 7, 6, 5];
+var c3 = [];
+console.log(arrayStringify(c1));
+console.log(arrayStringify(c2));
+console.log(arrayStringify(c3));
+module.exports = arrayStringify;
---
+++ q3.js
@@ -0,0 +1,20 @@
+function primeNumber(a) {
+    if (a <= 1) {
+        console.log('NO');
+        return;
+    }
+    for (var i = 2; i < a; i++) {
+        if (a % i === 0) {
+            console.log('NO');
+            return;
+        }
+    }
+    console.log('YES');
+}
+var d1 = 10;
+var d2 = 29;
+var d3 = 2;
+console.log(primeNumber(d1));
+console.log(primeNumber(d2));
+console.log(primeNumber(d3));
+module.exports = primeNumber;
